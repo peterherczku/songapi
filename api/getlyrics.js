668047b8047dc7ae66ff90894e40a6bf3bb0eac3
Peter Herczku <peterherczku915@gmail.com>
2024-12-05 17:17:03 +0100

    feat: added caching?

--- api/getlyrics.js
+++ api/getlyrics.js
@@ -1,22 +1,27 @@
 const Genius = require("genius-lyrics");
 const Client = new Genius.Client(
-  "RhhiJZGKjlALygOmujteGUe8iW8e6gnAgS3Sm96wzvWWmFtPOogXTmwSMhQTMYWS"
+	"RhhiJZGKjlALygOmujteGUe8iW8e6gnAgS3Sm96wzvWWmFtPOogXTmwSMhQTMYWS"
 );
 
+const cache = {};
+
 module.exports = async (req, res) => {
-  const { id } = req.query;
+	const { id } = req.query;
 
-  if (!id || isNaN(id)) {
-    return res
-      .status(400)
-      .json({ error: "The id parameter must be a valid number." });
-  }
-  const numericId = parseInt(id, 10);
-  const song = await Client.songs.get(numericId);
-  const lyrics = await song.lyrics();
-
-  res
-    .status(200)
-    .setHeader("Access-Control-Allow-Origin", "*")
-    .json({ artist: song.artist.name, title: song.title, lyrics: lyrics });
+	if (!id || isNaN(id)) {
+		return res
+			.status(400)
+			.json({ error: "The id parameter must be a valid number." });
+	}
+	if (cache[id]) {
+		return cache[id];
+	}
+	const numericId = parseInt(id, 10);
+	const song = await Client.songs.get(numericId);
+	const lyrics = await song.lyrics();
+	cache[id] = lyrics;
+	res
+		.status(200)
+		.setHeader("Access-Control-Allow-Origin", "*")
+		.json({ artist: song.artist.name, title: song.title, lyrics: lyrics });
 };
--- api/getsongs.js
+++ api/getsongs.js
@@ -1,24 +1,24 @@
 const Genius = require("genius-lyrics");
 const Client = new Genius.Client(
-  "RhhiJZGKjlALygOmujteGUe8iW8e6gnAgS3Sm96wzvWWmFtPOogXTmwSMhQTMYWS"
+	"RhhiJZGKjlALygOmujteGUe8iW8e6gnAgS3Sm96wzvWWmFtPOogXTmwSMhQTMYWS"
 );
 
 module.exports = async (req, res) => {
-  const { title, artist } = req.query;
-  if (!title) {
-    return res.status(400).json({ error: "Title is required" });
-  }
-  const searches = await Client.songs.search(title + (artist ?? ""));
+	const { title, artist } = req.query;
+	if (!title) {
+		return res.status(400).json({ error: "Title is required" });
+	}
+	const searches = await Client.songs.search(title + (artist ?? ""));
 
-  // Pick first one
-  const results = searches.map((search) => ({
-    id: search.id,
-    title: search.title,
-    artist: search.artist.name,
-  }));
+	// Pick first one
+	const results = searches.map((search) => ({
+		id: search.id,
+		title: search.title,
+		artist: search.artist.name,
+	}));
 
-  res
-    .status(200)
-    .setHeader("Access-Control-Allow-Origin", "*")
-    .json({ searchResults: results });
+	res
+		.status(200)
+		.setHeader("Access-Control-Allow-Origin", "*")
+		.json({ searchResults: results });
 };
